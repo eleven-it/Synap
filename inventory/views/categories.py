0d2a286e37235a39d140e862cb82a0a956151590
Sebastian <sebastian@MacBook-Air-de-Sebastian.local>
2025-06-30 13:42:11 -0300

    Actualización automática desde el servidor

--- inventory/views/categories.py
+++ inventory/views/categories.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
+from django.http import JsonResponse
 from ..models import Category
 
 class CategoryListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
@@ -12,27 +13,61 @@ class CategoryListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
 
 class CategoryCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Category
-    template_name = 'inventory/category_form.html'
     fields = ['name', 'is_active']
     permission_required = 'inventory.add_category'
     success_url = reverse_lazy('inventory:category_list')
+
+    def get_template_names(self):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest' or self.request.GET.get('modal') == '1':
+            return ['inventory/category_form_modal.html']
+        return ['inventory/category_form.html']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = "Create Category"
         return context
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest' or self.request.GET.get('modal') == '1':
+            return JsonResponse({'success': True, 'id': self.object.id, 'name': self.object.name})
+        return response
+
+    def form_invalid(self, form):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest' or self.request.GET.get('modal') == '1':
+            from django.template.loader import render_to_string
+            html = render_to_string('inventory/category_form_modal.html', {'form': form, 'title': 'Create Category'}, request=self.request)
+            return JsonResponse({'success': False, 'html': html})
+        return super().form_invalid(form)
+
 class CategoryUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Category
-    template_name = 'inventory/category_form.html'
     fields = ['name', 'is_active']
     permission_required = 'inventory.change_category'
     success_url = reverse_lazy('inventory:category_list')
+
+    def get_template_names(self):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest' or self.request.GET.get('modal') == '1':
+            return ['inventory/category_form_modal.html']
+        return ['inventory/category_form.html']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = "Edit Category"
         return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest' or self.request.GET.get('modal') == '1':
+            return JsonResponse({'success': True, 'id': self.object.id, 'name': self.object.name})
+        return response
+
+    def form_invalid(self, form):
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest' or self.request.GET.get('modal') == '1':
+            from django.template.loader import render_to_string
+            html = render_to_string('inventory/category_form_modal.html', {'form': form, 'title': 'Edit Category'}, request=self.request)
+            return JsonResponse({'success': False, 'html': html})
+        return super().form_invalid(form)
 
 class CategoryDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Category
